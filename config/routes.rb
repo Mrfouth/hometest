51bd94938c28d4b1577707e2d2963244e612bc0c
Jesse Dong <jesse.dong@tbwa.com>
2018-03-21 11:05:09 +0800

    users scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'users#index'
+
+  get '/404' => 'errors#error_404'
+  get '/422' => 'errors#error_422'
+  get '/500' => 'errors#error_500'
 end
